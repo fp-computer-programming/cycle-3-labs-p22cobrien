cf93b4163884738ce201c1e5f5332c8135d48636
p22cobrien <p22cobrien@fairfieldprep.org>
2021-09-28 10:03:58 -0400

    submit cycle 3 day 2 labs

---
+++ lab_2-1.py
@@ -0,0 +1,6 @@
+# Author: CMOB 9/28/2021
+
+x = input("insert a number: ")
+
+if (int(x) % 2) == 0:
+    print(x + " is an even number.")
---
+++ lab_2-2.py
@@ -0,0 +1,8 @@
+# Author: CMOB 9/28/2021
+
+x = input("Insert a number: ")
+
+if (int(x) % 2) == 0:
+    print(x + " is an even number.")
+else:
+    print(x + " is an odd number.")
