dbded0f894437ef33949bd0bfe70bfb670eb5a42
p22cobrien <p22cobrien@fairifeldprep.org>
2021-10-05 16:26:33 -0400

    sumbit lab_5-1.py

---
+++ lab_5-1.py
@@ -0,0 +1,16 @@
+# Author: CMOB 5/10/2021
+
+int(a)
+# name error
+
+int('a')
+# value error
+
+'a' + 2
+# type error
+
+import date
+# module not found error
+
+print("I'm a happy camper!)
+#syntax error
