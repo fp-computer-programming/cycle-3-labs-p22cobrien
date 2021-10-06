c94eb7b963705cad509c86f01d82432924d3bf59
p22cobrien <p22cobrien@fairfieldprep.org>
2021-10-06 10:02:44 -0400

    Auto stash before merge of "master" and "origin/master"

--- lab_5-1.py
+++ lab_5-1.py
@@ -1,16 +1,16 @@
 # Author: CMOB 5/10/2021
 
-int(a)
+# int(a)
 # name error
 
-int('a')
+# int('a')
 # value error
 
-'a' + 2
+# 'a' + 2
 # type error
 
-import date
+# import date
 # module not found error
 
-print("I'm a happy camper!)
-#syntax error
+# print("I'm a happy camper!)
+# syntax error
